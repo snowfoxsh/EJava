7763e36e3c07b2279416ee404f6d3049c2d38b4b
stormy482 <storm1surge@gmail.com>
2021-09-29 12:15:43 -0700

    [Pat] Added Engine class

---
+++ main/src/Engine.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Engine {
+
+    public Engine() {
+        // INIT
+    }
+
+    public void begin() {
+        // start engine
+    }
+
+    public void start() {
+        // start game loop
+    }
+
+
+
+}
